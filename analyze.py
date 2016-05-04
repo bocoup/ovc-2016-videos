39d6d6daf39cc7fb9a911351acaf2d0d88a02123
Peter Beshai <peter.beshai@gmail.com>
2016-05-04 17:13:14 -0400

    Switch to 4 space indentation

--- analyze.py
+++ analyze.py
@@ -6,27 +6,27 @@ from nltk.corpus import stopwords
 from collections import Counter
 
 files = [
-  "transcripts_no_timestamp/ovc2016_25_01_wattenberg_viegas.txt",
-  "transcripts_no_timestamp/ovc2016_25_02_mcdonald.txt",
-  "transcripts_no_timestamp/ovc2016_25_03_kosaka.txt",
-  "transcripts_no_timestamp/ovc2016_25_04_case.txt",
-  "transcripts_no_timestamp/ovc2016_25_05_mcnamara.txt",
-  "transcripts_no_timestamp/ovc2016_25_06_bremer.txt",
-  "transcripts_no_timestamp/ovc2016_25_07_vivo.txt",
-  "transcripts_no_timestamp/ovc2016_25_08_armstrong.txt",
-  "transcripts_no_timestamp/ovc2016_25_09_wu.txt",
-  "transcripts_no_timestamp/ovc2016_25_10_chu.txt",
-  "transcripts_no_timestamp/ovc2016_26_01_chalabi.txt",
-  "transcripts_no_timestamp/ovc2016_26_02_yanofsky.txt",
-  "transcripts_no_timestamp/ovc2016_26_03_elliott.txt",
-  "transcripts_no_timestamp/ovc2016_26_04_binx.txt",
-  "transcripts_no_timestamp/ovc2016_26_05_satyanarayan.txt",
-  "transcripts_no_timestamp/ovc2016_26_06_pearce.txt",
-  "transcripts_no_timestamp/ovc2016_26_07_hullman.txt",
-  "transcripts_no_timestamp/ovc2016_26_08_albrecht.txt",
-  "transcripts_no_timestamp/ovc2016_26_09_waigl.txt",
-  "transcripts_no_timestamp/ovc2016_26_10_collins.txt",
-  "transcripts_no_timestamp/ovc2016_26_11_becker.txt"
+    "transcripts_no_timestamp/ovc2016_25_01_wattenberg_viegas.txt",
+    "transcripts_no_timestamp/ovc2016_25_02_mcdonald.txt",
+    "transcripts_no_timestamp/ovc2016_25_03_kosaka.txt",
+    "transcripts_no_timestamp/ovc2016_25_04_case.txt",
+    "transcripts_no_timestamp/ovc2016_25_05_mcnamara.txt",
+    "transcripts_no_timestamp/ovc2016_25_06_bremer.txt",
+    "transcripts_no_timestamp/ovc2016_25_07_vivo.txt",
+    "transcripts_no_timestamp/ovc2016_25_08_armstrong.txt",
+    "transcripts_no_timestamp/ovc2016_25_09_wu.txt",
+    "transcripts_no_timestamp/ovc2016_25_10_chu.txt",
+    "transcripts_no_timestamp/ovc2016_26_01_chalabi.txt",
+    "transcripts_no_timestamp/ovc2016_26_02_yanofsky.txt",
+    "transcripts_no_timestamp/ovc2016_26_03_elliott.txt",
+    "transcripts_no_timestamp/ovc2016_26_04_binx.txt",
+    "transcripts_no_timestamp/ovc2016_26_05_satyanarayan.txt",
+    "transcripts_no_timestamp/ovc2016_26_06_pearce.txt",
+    "transcripts_no_timestamp/ovc2016_26_07_hullman.txt",
+    "transcripts_no_timestamp/ovc2016_26_08_albrecht.txt",
+    "transcripts_no_timestamp/ovc2016_26_09_waigl.txt",
+    "transcripts_no_timestamp/ovc2016_26_10_collins.txt",
+    "transcripts_no_timestamp/ovc2016_26_11_becker.txt"
 ]
 
 # Many of these functions are taken from https://github.com/bocoup-education/text-vis-ovc
@@ -36,17 +36,17 @@ files = [
 # input: string or list of tokens to remove
 # output: list of tokens with remove_tokens removed
 def remove_tokens(tokens, remove_tokens):
-  return [token for token in tokens if token not in remove_tokens]
+    return [token for token in tokens if token not in remove_tokens]
 
 # read text from a file and tokenize via nltk word_tokenize
 def words_from_file(filename):
-  with open(filename) as handle:
-    text = handle.read()
+    with open(filename) as handle:
+        text = handle.read()
 
-  # remove newlines
-  text = re.sub(r'\n', ' ', text)
+    # remove newlines
+    text = re.sub(r'\n', ' ', text)
 
-  return nltk.word_tokenize(text)
+    return nltk.word_tokenize(text)
 
 # input: list of tokens
 # output: list of tokens with every token having only lowercase letters.
@@ -59,12 +59,12 @@ def remove_word_fragments(tokens):
 
 # prepare tokens by removing punctuation, making lowercase and removing stop words
 def prepare_tokens(tokens):
-  stops = stopwords.words('english')
-  tokens = remove_tokens(tokens, punctuation + "--")
-  tokens = lowercase(tokens)
-  tokens = remove_tokens(tokens, stops)
-  tokens = remove_word_fragments(tokens)
-  return tokens
+    stops = stopwords.words('english')
+    tokens = remove_tokens(tokens, punctuation + "--")
+    tokens = lowercase(tokens)
+    tokens = remove_tokens(tokens, stops)
+    tokens = remove_word_fragments(tokens)
+    return tokens
 
 # input: list of tokens
 # returns: dict of counts
@@ -91,7 +91,6 @@ def document_frequency(token, corpus_tokens):
     '''Returns number of times a token appears in a set of documents'''
     doc_count = 0
     for tokens in corpus_tokens:
-
         if token in tokens:
             doc_count += 1
 
@@ -109,7 +108,6 @@ def inverse_doc_frequency(token, corpus_tokens):
 #  One list for each document in the corpora.
 # output: list of (token, tf-idf) values for each unique token in document_tokens
 def tf_idf(document_tokens, corpus_tokens):
-
 
     # Get our token frequencies for all the unique tokens in our document
     token_counts = get_counts(document_tokens)
@@ -132,18 +130,18 @@ transcript_tokens = []
 
 # tokenize all the transcripts
 for filename in files:
-  words = prepare_tokens(words_from_file(filename))
+    words = prepare_tokens(words_from_file(filename))
 
-  # add list of tokens for talk to collection
-  transcript_tokens.append(words)
+    # add list of tokens for talk to collection
+    transcript_tokens.append(words)
 
 
 # compute tf-idf and output results to console
 for i, file in enumerate(files):
-  token_tf_idfs = tf_idf(transcript_tokens[i], transcript_tokens)
-  sorted_token_tf_idfs = sort_counts(token_tf_idfs)
+    token_tf_idfs = tf_idf(transcript_tokens[i], transcript_tokens)
+    sorted_token_tf_idfs = sort_counts(token_tf_idfs)
 
-  print(file)
-  for [token, value] in sorted_token_tf_idfs[0:20]:
-    print('{},{}'.format(token, value))
-  print('---------\n')
+    print(file)
+    for [token, value] in sorted_token_tf_idfs[0:20]:
+        print('{},{}'.format(token, value))
+    print('---------\n')
