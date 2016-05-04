a01f301198f9fc35720477428c689d3a38ed229e
Peter Beshai <peter.beshai@gmail.com>
2016-05-04 17:51:33 -0400

    Add in bigram analysis

---
+++ analysis/analyze_bigrams.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk.collocations import BigramCollocationFinder
+from utils import tokenize_transcripts, get_files
+
+# a list of tokens for each of the talks
+transcript_tokens = tokenize_transcripts()
+
+# built in bigram metrics are in here
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+
+# compute top bigrams and output results to console
+for i, file in enumerate(get_files()):
+    finder = BigramCollocationFinder.from_words(transcript_tokens[i])
+    bigrams = finder.score_ngrams(bigram_measures.likelihood_ratio)[:20]
+
+    print(file)
+    for [tokens, value] in bigrams[0:20]:
+        print('{},{}'.format(" ".join(tokens), value))
+    print('---------\n')
