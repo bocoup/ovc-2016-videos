4c1943176409443e6d5e3e7498157bbd4988d65b
Peter Beshai <peter.beshai@gmail.com>
2016-05-04 18:00:00 -0400

    Add in trigrams analysis

---
+++ analysis/analyze_trigrams.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk.collocations import TrigramCollocationFinder
+from utils import tokenize_transcripts, get_files
+
+# a list of tokens for each of the talks
+transcript_tokens = tokenize_transcripts()
+
+# built in trigram metrics are in here
+trigram_measures = nltk.collocations.TrigramAssocMeasures()
+
+# compute top trigrams and output results to console
+for i, file in enumerate(get_files()):
+    finder = TrigramCollocationFinder.from_words(transcript_tokens[i])
+    trigrams = finder.score_ngrams(trigram_measures.likelihood_ratio)[:20]
+
+    print(file)
+    for [tokens, value] in trigrams[0:20]:
+        print('{},{}'.format(" ".join(tokens), value))
+    print('---------\n')
