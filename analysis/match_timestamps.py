e77667e135c37648fceab3ee177004bae8aacb28
Peter Beshai <peter.beshai@gmail.com>
2016-05-06 15:26:26 -0400

    Filter out empty lines, create one big dictionary to hold tokens and bigrams

--- analysis/match_timestamps.py
+++ analysis/match_timestamps.py
@@ -30,20 +30,29 @@ start_time = time_to_delta_seconds(lines[0][0], 0)
 # convert timestamp to delta seconds, tokenize the line as individual words
 lines = [(time_to_delta_seconds(line[0], start_time), tokenize_line(line[1])) for line in lines]
 
-
-# map words to timestamps
-tokens_to_timestamps = {}
-for line in lines:
-    timestamp = line[0]
-    tokens = line[1]
-    for token in tokens:
-        if token in tokens_to_timestamps:
-            if timestamp not in tokens_to_timestamps[token]:
-                tokens_to_timestamps[token].append(timestamp)
-        else:
-            tokens_to_timestamps[token] = [timestamp]
+# filter out empty lines
+lines = [line for line in lines if line[1]]
 
 
+# map words to timestamps in a dictionary
+def map_terms_to_timestamps(lines):
+    terms_to_timestamps = {}
+    for line in lines:
+        timestamp = line[0]
+        tokens = line[1]
+        for token in tokens:
+            if token in terms_to_timestamps:
+                if timestamp not in terms_to_timestamps[token]:
+                    terms_to_timestamps[token].append(timestamp)
+            else:
+                terms_to_timestamps[token] = [timestamp]
+
+    return terms_to_timestamps
+
+
+
+# map tokens to timestamps
+tokens_to_timestamps = map_terms_to_timestamps(lines)
 
 # BIGRAMS
 # - both words on one line
@@ -52,7 +61,7 @@ for line in lines:
 # ==> if we make a "line" = line + first word of next line, it should be good
 
 ## ORRR we already have it mapped to timestamp, list of tokens L
-# we can change that to timestamp, (L[0][0] + L[0][1], ..., L[0][N] + L[1][0])
+# so we can change that to timestamp, (L[0][0] + L[0][1], ..., L[0][N] + L[1][0])
 bigram_lines = []
 num_lines = len(lines)
 for i, line in enumerate(lines):
@@ -68,9 +77,14 @@ for i, line in enumerate(lines):
     if i + 1 < num_lines and num_tokens > 0:
         next_line = lines[i + 1]
         next_line_tokens = next_line[1]
-        if len(next_line_tokens) > 0:
-            bigrams.append("{} {}".format(tokens[num_tokens - 1], next_line_tokens[0]))
+        bigrams.append("{} {}".format(tokens[num_tokens - 1], next_line_tokens[0]))
 
     bigram_lines.append((timestamp, bigrams))
 
-print(bigram_lines)
+bigrams_to_timestamps = map_terms_to_timestamps(bigram_lines)
+
+# merge the timestamp dictionaries for both bigrams and individual word tokens
+terms_to_timestamps = {**tokens_to_timestamps, **bigrams_to_timestamps}
+print(terms_to_timestamps)
+
+
