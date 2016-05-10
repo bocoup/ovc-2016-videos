9e83eac24b34b49176e3cb9ef57b533bc302e15f
Peter Beshai <peter.beshai@gmail.com>
2016-05-10 17:51:27 -0400

    Separate terms and frames into their own JSON files to facilitate regeneration

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -8,10 +8,20 @@ import TalkPane from './TalkPane';
 
 import './Main.scss';
 import ovcData from '../data/ovc2016_talks.json';
+import ovcTermsData from '../data/ovc2016_terms.json';
+import ovcFramesData from '../data/ovc2016_frames.json';
 
 // for convenience while debugging, put d3 and the data in window
 window.d3 = d3;
 window.ovcData = ovcData;
+console.log('ovcTermsData', ovcTermsData);
+
+// merge terms into talks
+ovcData.forEach((talk, i) => {
+  talk.terms = ovcTermsData[i].terms;
+  talk.frames = ovcFramesData[i];
+});
+
 console.log('ovcData =', ovcData);
 
 const Main = React.createClass({
