a6912d1d768d4979dc96f18fd2d54ee7228a2dc5
Peter Beshai <peter.beshai@gmail.com>
2016-05-11 14:18:37 -0400

    Re-style grid cells and include links to youtube and slides

--- app/src/components/TalkGridCell.js
+++ app/src/components/TalkGridCell.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import d3 from 'd3';
 import './TalkGridCell.scss';
 
 const TalkGridCell = React.createClass({
@@ -18,10 +18,35 @@ const TalkGridCell = React.createClass({
   render() {
     const { data } = this.props;
 
+    if (!data) {
+      return null;
+    }
+
+    const totalNumTerms = 5;
+    const numTop = 2;
+    const topTerms = data.terms.slice(0, numTop);
+
+    // add in a few random terms
+    const remainingTerms = data.terms.slice(numTop);
+    const terms = topTerms.concat(d3.shuffle(remainingTerms).slice(0, totalNumTerms - numTop));
+
+    const hasSlides = (data.slidesUrl && data.slidesUrl) !== '';
+
     return (
       <div className='talk-grid-cell' onClick={this._handleSelectTalk}>
-        <h1 className='talk-title'>{data.title}</h1>
-        <h2 className='talk-speakers'>{data.speakers}</h2>
+        <div className='talk-grid-cell-main'>
+          <h1 className='talk-title'>{data.title}</h1>
+          <h2 className='talk-speakers'>{data.speakers}</h2>
+          <div className='talk-terms'>
+            {terms.map((term, i) => {
+              return <div key={i} className='talk-term'>{term.term}</div>;
+            })}
+          </div>
+        </div>
+        <div className='talk-links'>
+          <a href={`https://www.youtube.com/watch?v=${data.youtubeId}`}><i className='fa fa-lg fa-youtube-play' title='Play Video' /></a>
+          {hasSlides ? <a href={`${data.slidesUrl}`}><i className='fa fa-lg fa-desktop' title='View Slides' /></a> : null}
+        </div>
       </div>
     );
   }
