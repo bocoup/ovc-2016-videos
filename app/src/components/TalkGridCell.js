2924b402e0c5fd4c82b982ccf6b7208aa800f84f
Peter Beshai <peter.beshai@gmail.com>
2016-05-12 14:38:42 -0400

    Add in changing the random terms over time on the grid cells

--- app/src/components/TalkGridCell.js
+++ app/src/components/TalkGridCell.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import d3 from 'd3';
 import './TalkGridCell.scss';
+import TimeoutTransitionGroup from '../vendor/TimeoutTransitionGroup';
+
+function timerDelay() {
+  return Math.random() * 30000 + 15000;
+}
+
+const transitionTime = 1000;
 
 const TalkGridCell = React.createClass({
   propTypes: {
     data: React.PropTypes.object,
     onSelectTalk: React.PropTypes.func
+  },
+
+  getInitialState() {
+    return {
+      terms: this._selectTerms()
+    };
+  },
+
+  componentWillMount() {
+    this.timer = setTimeout(() => this._timedSetTerms(), timerDelay());
+  },
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+    this.timer = null;
   },
 
   _handleSelectTalk() {
@@ -15,6 +37,35 @@ const TalkGridCell = React.createClass({
     }
   },
 
+  // one timer removes exisitng random, then the next adds in a new set of random
+  _timedSetTerms(includeRandomTerms) {
+    this.timer = null;
+    const terms = this._selectTerms(includeRandomTerms);
+    this.setState({ terms });
+
+    // if we included random terms, keep this for the long delay, otherwise just wait for the leave
+    // transition to finish then add in random words
+    this.timer = setTimeout(() => this._timedSetTerms(!includeRandomTerms),
+      includeRandomTerms ? timerDelay() : transitionTime);
+  },
+
+  _selectTerms(includeRandomTerms) {
+    const { data } = this.props;
+
+    const totalNumTerms = 6;
+    const numTop = 3;
+    const topTerms = data.terms.slice(0, numTop);
+    let terms = topTerms;
+
+    // add in a few random terms
+    if (includeRandomTerms) {
+      const remainingTerms = data.terms.slice(numTop);
+      terms = topTerms.concat(d3.shuffle(remainingTerms).slice(0, totalNumTerms - numTop));
+    }
+
+    return terms;
+  },
+
   render() {
     const { data } = this.props;
 
@@ -22,13 +73,7 @@ const TalkGridCell = React.createClass({
       return null;
     }
 
-    const totalNumTerms = 6;
-    const numTop = 3;
-    const topTerms = data.terms.slice(0, numTop);
-
-    // add in a few random terms
-    const remainingTerms = data.terms.slice(numTop);
-    const terms = topTerms.concat(d3.shuffle(remainingTerms).slice(0, totalNumTerms - numTop));
+    const { terms } = this.state;
 
     const hasSlides = (data.slidesUrl && data.slidesUrl) !== '';
 
@@ -41,9 +86,12 @@ const TalkGridCell = React.createClass({
           <h1 className='talk-title'>{data.title}</h1>
           <h2 className='talk-speakers'>{data.speakers}</h2>
           <div className='talk-terms'>
-            {terms.map((term, i) => {
-              return <div key={i} className='talk-term' style={{ opacity: scoreScale(term.score) }}>{term.term}</div>;
-            })}
+            <TimeoutTransitionGroup component='div' transitionName='talk-term' enterTimeout={transitionTime} leaveTimeout={transitionTime}>
+              {terms.map((term) => {
+                // wrap it in a div to get the transition opacity
+                return <div className='talk-term-container' key={term.term}><div className='talk-term' style={{ opacity: scoreScale(term.score) }}>{term.term}</div></div>;
+              })}
+            </TimeoutTransitionGroup>
           </div>
         </div>
         <div className='talk-grid-cell-bottom'>
